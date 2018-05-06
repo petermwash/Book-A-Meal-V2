3092d5d7fe13e3f2e7dccfe54dfe638569f6c3bc
petermwash <petermwash48@gmail.com>
2018-05-06 15:47:35 +0300

    FEATURE: Secure endpoints with JWT

--- app/__init__.py
+++ app/__init__.py
@@ -1,79 +1,172 @@
-from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify, abort
 from flask_restful import Resource, Api
+import uuid
+import jwt
+import datetime
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
 
 """here are my local imports"""
 from instance.config import app_config
-#from .models import db
+
 
 """here am initializing sql-alchemy"""
 db = SQLAlchemy()
 
 def create_app(config_name):
-	from app.models import User, Meal, Order, OrderDetails, Menu
+	from app.models import User, Meal, Order, Menu
 
-	app = FlaskAPI(__name__, instance_relative_config=True)
+	app = Flask(__name__)
 	app.config.from_object(app_config[config_name])
-	app.config.from_pyfile('config.py')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	app.config['SECRET_KEY'] = "thisismysupersevrectkeythatyoucannevverguewhatitis"
 	api = Api(app)
 	db.init_app(app)
 
+	def token_required(f):
+		 @wraps(f)
+		 def decorated(*args, **kwargs):
+		 	access_token = None
+
+		 	if 'x-access-token' in request.headers:
+		 		access_token = request.headers['x-access-token']
+
+		 	if not access_token:
+		 		response = jsonify({"message": "Token is  mising!. You need to sign in first"})
+		 		response.status_code = 401
+		 		return response
+
+		 	try:
+		 		data = jwt.decode(access_token, app.config['SECRET_KEY'])
+		 		current_user = User.query.filter_by(public_id=data['public_id']).first()
+		 	except:
+		 		response = jsonify({"message": "Token is invalid!. You need to sign in first"})
+		 		response.status_code = 401
+		 		return response
+
+		 	return f(current_user, *args, **kwargs)
+
+		 return decorated
+
 	class UserSignup(Resource):
+		"""This class contains signup resource"""
+		
 		def post(self):
 			f_name = request.json.get('f_name')
 			l_name = request.json.get('l_name')
 			email = request.json.get('email')
 			u_name = request.json.get('u_name')
 			password = request.json.get('password')
+
 			if u_name is None or password is None or f_name is None\
 					or l_name is None or email is None: 
 				response = jsonify({"message": "Missing argument"})
 				response.status_code = 400
 				return response
+
 			users = User.query.all()
 			for user in users:
-				if user.u_name == u_name:
+				if user.u_name == u_name or user.email == email:
 					response = jsonify({"message": "User already exists!"})
-					response.status_code = 405
+					response.status_code = 202
 					return response
-			uzer = User(f_name, l_name, email, u_name, password)
+
+			hashed_pasword = generate_password_hash(password, method='sha256')
+			public_id = str(uuid.uuid4())
+
+			uzer = User(public_id, f_name, l_name, email, u_name, hashed_pasword)
 			uzer.save()
-			response = jsonify({"message": "User created"})
+			response = jsonify({"message": "New user created!"})
 			response.status_code = 201
 			return response
 
 	class UserSignin(Resource):
+		"""This class contains signin resource"""
+		
 		def post(self):
 			u_name = request.json.get('u_name')
 			password = request.json.get('password')
-			users = User.query.all()
-			for user in users:
-				if user.u_name == u_name:
-					if user.password == password:
-						response = jsonify({"message": "Succefully logged in"})
-						response.status_code = 200
-						return response
-					"""verify_password(u_name, password)
-					token = g.user.generate_auth_token(600)
-					response = jsonify({"token": token.decode('ascii'),
-										"duration": 600,
-										"message": "User loged in"})"""
-					#return response
-				response = jsonify({
-								"message": "Wrong password or username"
-								})
+			#auth = request.authorization
+
+			#if not auth or not auth.username or not auth.password:
+			if not u_name or not password:
+				response = jsonify({"message": "can\'t verify"})
 				response.status_code = 401
 				return response
 
-	class UpgradeUser():
-		def put():
-			pass
+			current_user = User.query.filter_by(u_name=u_name).first()
+			
+			if not current_user:
+				response = jsonify({"message": "User {} doesn\'t exist".format(u_name)})
+				response.status_code = 404
+				return response
+
+			if check_password_hash(current_user.password, password):
+				access_token = jwt.encode({'public_id': current_user.public_id, 'exp': datetime.datetime.utcnow()
+					+ datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+				response = jsonify({
+					"message": "Logged in as {}".format(current_user.u_name),
+					"access_token": access_token.decode('UTF-8')})
+				response.status_code = 200
+				return response
+			else:
+				response = jsonify({"message": "Wrong password or username!"})
+				response.status_code = 401
+				return response
+
+
+	class UpgradeUser(Resource):
+		"""This class contains resource  to give a user admin rights"""
+		
+		@token_required
+		def put(current_user, self, id):
+			user = User.query.filter_by(id=id).first()
+
+			if not user:
+				response = jsonify({"message": "User {} doesn\'t exist".format(id)})
+				response.status_code = 404
+				return response
+
+			user.type_admin = True
+			user.save()
+			response = jsonify({"message": "User {} promoted to admin".format(user.u_name)})
+			response.status_code = 200
+			return response
+
+	class Users(Resource):
+		"""This class is for inspecting users"""
+		
+		@token_required
+		def get(current_user, self):
+			users = User.query.all()
+			uzers = []
+
+			for user in users:
+				if not user:
+					return jsonify({"message": "No user found!"})
+
+				u = {
+					"id": user.id,
+					"u_name": user.u_name,
+					"public_id": user.public_id,
+					"type_admin": user.type_admin
+				}
+				uzers.append(u)
+
+			return uzers
 
 	class Meals(Resource):
-		def get(self):
-			meals = Meal.query.all() #get_all()
+		"""This class contains resources for maniputating meals"""
+
+		@token_required
+		def get(current_user, self):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+
+			meals = Meal.query.all()
 			res = []
 
 			for meal in meals:
@@ -92,21 +185,30 @@ def create_app(config_name):
 			response.status_code = 200
 			return response
 
-		def post(self):
+		@token_required
+		def post(current_user, self):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+
 			m_name = request.json.get('m_name')
 			category = request.json.get('category')
 			price = request.json.get('price')
+
 			if m_name is None or category is None\
 					or price is None: 
 				response = jsonify({"message": "Missing argument!"})
 				response.status_code = 400
 				return response
+
 			meals = Meal.query.all()
 			for meal in meals:
 				if meal.m_name == m_name:
 					response = jsonify({"message": "Meal already exists!"})
 					response.status_code = 405
 					return response
+
 			meal = Meal(m_name, category, price)
 			meal.save()
 			response = jsonify({"message": "Meal added"})
@@ -114,29 +216,48 @@ def create_app(config_name):
 			return response
 
 	class SingleMeal(Resource):
+		"""This class contains resource for maniputating a single meal"""
 
-		def put(self, meal_id):
+		@token_required
+		def put(current_user, self, meal_id):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+
 			meal = Meal.query.filter_by(id=meal_id).first()
 			if not meal:
 				abort(404)
+
 			m_name = request.json.get('m_name')
 			category = request.json.get('category')
 			price = request.json.get('price')
+
 			if m_name is not None:
 				meal.m_name = m_name
+
 			if category is not None:
 				meal.category = category
+
 			if price is not None:
 				meal.price = price
+
 			meal.save()
 			response = jsonify({"message": "Meal updated"})
 			response.status_code = 200
 			return response
 
-		def get(self, meal_id):
+		@token_required
+		def get(current_user, self, meal_id):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+
 			meal = Meal.query.filter_by(id=meal_id).first()
 			if not meal:
 				abort(404)
+
 			response = jsonify({
 				'id': meal.id,
 				'm_name': meal.m_name,
@@ -146,125 +267,155 @@ def create_app(config_name):
 			response.status_code = 200
 			return response
 
-		def delete(self, meal_id):
+		@token_required
+		def delete(current_user, self, meal_id):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+			
 			meal = Meal.query.filter_by(id=meal_id).first()
 			if not meal:
 				abort(404)
+
 			meal.delete()
 			response = jsonify({"message": "Meal deleted!"})
 			response.status_code = 200
 			return response
 
 	class OrderEndpoints(Resource):
-		def post(self):
-			meal_id = request.json.get('meal_id')
+		"""This class contains resources for maniputating orders"""
+
+		@token_required
+		def post(current_user, self):
+			
 			owner = request.json.get('owner')
 			meal_name = request.json.get('meal_name')
 			quantity = request.json.get('quantity')
-			if owner is None or meal_id is None\
-					or quantity is None or meal_name is None: 
+
+			if owner is None or quantity is None or meal_name is None: 
 				response = jsonify({"message": "Missing argument"})
 				response.status_code = 400
 				return response
-			order = Order(meal_id, meal_name, quantity, owner)
+
+			order = Order(meal_name, quantity, owner)
 			order.save()
-			response = jsonify({"message": "Order created"})
+			response = jsonify({"message": "Order sccesfully posted"})
 			response.status_code = 201
 			return response
 
-		def get(self):
+		@token_required
+		def get(current_user, self):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+			
 			orders = Order.get_all()
 			res = []
 
-			for order in orders:
-				"""if not order:
-					response = jsonify({"message": "No meals available"})
-					response.status_code = 404
-					return response"""
-				meal_obj = {
-					'cart_id': order.cart_id,
-					'meal_name': order.meal_name,
-					'quantity': order.quantity,
-					'owner': order.owner
-				}
+			if not orders:
+				response = jsonify({"message": "No order available"})
+				response.status_code = 404
+				return response
+
+			for order in orders:	
+				meal_obj = {order.owner: {
+										'meal_name': order.meal_name,
+										'quantity': order.quantity
+
+										}
+					
+							}
 				res.append(meal_obj)
+
 			response = jsonify(res)
 			response.status_code = 200
 			return response
 
-		def put(self, order_id):
+		@token_required
+		def put(current_user, self, order_id):
+			
+			me = User.query.filter_by(u_name=current_user.u_name).first()
 			order = Order.query.filter_by(id=order_id).first()
+
+			if not me:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+			
 			if not order:
 				abort(404)
+
 			quantity = request.json.get('quantity')
 			if quantity is not None:
 				order.quantity = quantity
+
 			order.save()
 			response = jsonify({"message": "Order updated"})
-			response.status_code = 200
-			return response
-
-	class OrderDetsEndpoints(Resource):
-		def get(self):
-			o = OrderDetails.query.all()
-			r = []
-
-			for i in o:
-				ob = {
-				'id': o.id,
-				'order': o.order_cart,
-				'ordered_on': date_ordered
-				}
-				r.append(ob)
-			response = jsonify(r)
 			response.status_code = 200
 			return response
 
 
 	class MenuEndpoints(Resource):
 
-		def get(self):
-			menu = Menu.get_all()
-			res = []
+		"""This class contains resources for maniputating the menu"""
 
-			for meal in menu:
-				meal_obj = {
-							meal.id:{
-									'meal_name': meal.m_name,
-									'category': meal.category,
-									'price': meal.price
-									}
-							}
-				res.append(meal_obj)
-			
-			response = jsonify(res)
-			response.status_code = 200
-			return response
+		@token_required
+		def get(current_user, self):
 
-		def post(self, meal_id):
-			menu = Menu.get_all()
+				menu = Menu.get_all()
 
-			if not menu:
-				return jsonify({"message": "No meals available  in menu!"})
+				if not menu:
+					response = jsonify({"message": "No meals available  in menu!"})
+					response.status_code = 404
+					return response
+
+				res = []
+
+				for meal in menu:
+					meal_obj = {
+								meal.id:{
+										'meal_name': meal.m_name,
+										'category': meal.category,
+										'price': meal.price
+										}
+								}
+					res.append(meal_obj)
+				
+				response = jsonify(res)
+				response.status_code = 200
+				return response
+
+		@token_required
+		def post(current_user, self, meal_id):
+			if not current_user.type_admin:
+				response = jsonify({"message": "Not authorized to perform this function!"})
+				response.status_code = 401
+				return response
+
 			meal = Meal.query.filter_by(id=meal_id).first()
+				
 			if not meal:
 				response = jsonify({"message": "Meal not available"})
 				response.status_code = 404
 				return response
-			if meal.id == meal_id:
-				menus = Menu(meal.m_name, meal.category, meal.price)
-				menus.save()
-				response = jsonify({"message": "Meal added to menu"})
-				response.status_code = 201
-				return response
+				
+			menus = Menu(meal.m_name, meal.category, meal.price)
+			menus.save()
 
+			response = jsonify({"message": "Meal added to menu"})
+			response.status_code = 200
+			return response
 
-	api.add_resource(UserSignup, '/api/v2/signup')
-	api.add_resource(UserSignin, '/api/v2/signin')
+	api.add_resource(UserSignup, '/api/v2/auth/signup')
+	api.add_resource(UserSignin, '/api/v2/auth/login')
 	api.add_resource(Meals, '/api/v2/meals')
 	api.add_resource(SingleMeal, '/api/v2/meals/<int:meal_id>')
 	api.add_resource(MenuEndpoints, '/api/v2/menu', '/api/v2/menu/<int:meal_id>')
 	api.add_resource(OrderEndpoints, '/api/v2/orders', '/api/v2/orders/<int:order_id>')
-	api.add_resource(OrderDetsEndpoints, '/api/v2/orderst')
+	api.add_resource(UpgradeUser, '/api/v2/upgrade/<int:id>',)
+	api.add_resource(Users, '/api/v2/users')
+
 
 	return app
--- app/models.py
+++ app/models.py
@@ -1,43 +1,50 @@
+from flask_bcrypt import Bcrypt
+import jwt
+from datetime import datetime, timedelta
+from flask import current_app
+
+"""local imports"""
 from app import db
 
 class User(db.Model):
-	"""
-	This class represents a user table schema
-	"""
+	"""This class represents a user model schema"""
+
 	__tablename__ = 'users'
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	public_id = db.Column(db.String(75), nullable=False)
 	f_name = db.Column(db.String(75), nullable=False)
 	l_name = db.Column(db.String(75), nullable=False)
 	u_name = db.Column(db.String(75), unique=True, nullable=False)
 	email = db.Column(db.String(75), unique=True, nullable=False)
-	password = db.Column(db.String(75), nullable=False)
+	password = db.Column(db.String(225), nullable=False)
+	type_admin = db.Column(db.Boolean)
 	"""ordercart = db.relationship(
 		'Order', order_by='Order.cart_id', cascade="all, delete-orphan")"""
 
-	def __init__(self, f_name, l_name, email, u_name, password):
+	def __init__(self, public_id, f_name, l_name, email, u_name, password):
+		self.public_id = public_id
 		self.f_name = f_name
 		self.l_name = l_name
 		self.email = email
 		self.u_name = u_name
-		self.password = password
+		self.password = password 
 		self.type_admin = False
 
 	def save(self):
-		"""
-		Persist the user in the database
-		"""
+		"""Persist the user in the database"""
 		db.session.add(self)
 		db.session.commit()
 
+
 	def __repr__(self):
+		"""Return a representation of a user instance."""
 		return "<User: {}>".format(self.u_name)
 
 
 class Meal(db.Model):
-	"""
-	Class to represent the meal model
-	"""
+	"""Class to represent the meal model schema"""
+
 	__tablename__ = 'meals'
 	id  = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	m_name = db.Column(db.String(200), nullable=True)
@@ -50,9 +57,9 @@ class Meal(db.Model):
 		self.price = price
 
 	def save(self):
-		"""
-		Persist the meal option in the database
-		"""
+		
+		"""Persist the meal option in the database"""
+		
 		db.session.add(self)
 		db.session.commit()
 
@@ -65,23 +72,23 @@ class Meal(db.Model):
 		db.session.commit()
 
 	def __repr__(self):
+		"""Return a representation of meal instance."""
 		return "<Meal: {}>".format(self.m_name)
 
 class Order(db.Model):
 
-	"""This class defines the ordercart table."""
-	__tablename__ = 'ordercart'
+	"""This class defines the order model schema."""
+	__tablename__ = 'orders'
 
-	cart_id = db.Column(db.Integer, primary_key=True)
-	#meal = db.Column(db.Integer, db.ForeignKey(Meal.id))
-	meal_id = db.Column(db.Integer)#db.ForeignKey(Meal.id))
-	meal_name = db.Column(db.String(255))
+	id = db.Column(db.Integer, primary_key=True)
+	meal_name = db.Column(db.String(75), nullable=False)
 	quantity = db.Column(db.Integer)
-	owner = db.Column(db.Integer)#db.ForeignKey(User.id))
-
-	def __init__(self, meal_id, meal_name, quantity, owner):
+	owner = db.Column(db.String(75), nullable=False)
+	
+	def __init__(self, meal_name, quantity, owner):
+		
 		"""Initialize an order with a meal_name, quantity, and its owner."""
-		self.meal_id = meal_id
+		
 		self.meal_name = meal_name
 		self.quantity = quantity
 		self.owner = owner
@@ -92,52 +99,19 @@ class Order(db.Model):
 
 	@staticmethod
 	def get_all():
+
+		"""This method gets all the orders"""
+		
 		return Order.query.all()
-		"""This method gets all the orders for a given user."""
-		#return Order.query.filter_by(owner=user_id)
-	
-	def delete(self):
-		"""Deletes a given order."""
-		db.session.delete(self)
-		db.session.commit()
 
 	def __repr__(self):
 		"""Return a representation of order instance."""
 		return "<Order: {}>".format(self.meal_name)
 
-class OrderDetails(db.Model):
-
-	"""This class defines the ordercart table."""
-	
-	__tablename__ = 'orders'
-
-	id = db.Column(db.Integer, primary_key=True)
-	order_cart = db.Column(db.Integer, db.ForeignKey(Order.cart_id))
-	date_ordered = db.Column(db.DateTime, default=db.func.current_timestamp())
-	#owner = db.Column(db.Integer, db.ForeignKey(User.id))
-
-	def __init__(self):
-		"""Initialize an order with a meal_name, quantity, and its owner."""
-		self.order_cart = order_cart
-		self.date_ordered = date_ordered
-
-	def save(self):
-		db.session.add(self)
-		db.session.commit()
-
-	@staticmethod
-	def get_all(user_id):
-		"""This method gets all the orders details."""
-		return OrderDetails.query.all()
-	
-	def __repr__(self):
-		"""Return a representation of order instance."""
-		return "<OrderDetails: {}>".format(self.meal_name)
 
 class Menu(db.Model):
-	"""
-	Class to represent the menu model
-	"""
+	"""Class to represent the menu model schema"""
+
 	__tablename__ = 'menus'
 	id  = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	m_name = db.Column(db.String(200), nullable=False)
@@ -150,20 +124,19 @@ class Menu(db.Model):
 		self.price = price
 
 	def save(self):
-		"""
-		Persist the menu meal in the database
-		"""
+		
+		"""Persist the menu meal in the database"""
+		
 		db.session.add(self)
 		db.session.commit()
 
 	@staticmethod
 	def get_all():
+		
 		return Menu.query.all()
 
-	def delete(self):
-		db.session.delete(self)
-		db.session.commit()
 
 	def __repr__(self):
+		"""Return a representation of menu instance."""
 		return "<Menu: {}>".format(self.m_name)
 
