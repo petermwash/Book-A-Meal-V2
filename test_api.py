f3ae4c75ee61dede6287d15fb19074ba367ce925
petermwash <petermwash48@gmail.com>
2018-05-07 07:16:36 +0300

    CHORE: Add more test cases

--- test_api.py
+++ test_api.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+import uuid
 
 """here are my local imports"""
 from app import create_app, db
@@ -18,7 +19,7 @@ class UserTestCase(unittest.TestCase):
 			'f_name': 'Peter',
 			'l_name': 'Mwaura',
 			'u_name': 'Pemwa',
-			'email': 'pemwa@me.com',
+			'email': 'pemwa@gm.com',
 			'password': '12345'
 		}
 
@@ -27,19 +28,23 @@ class UserTestCase(unittest.TestCase):
 		and creating all tables
 		"""
 		with self.app.app_context():
+			db.session.close()
+			db.drop_all()
 			db.create_all()
 
 	def test_user_creation(self):
+		"""Test that API can register a user"""
 		response = self.client().post(
-			'/api/v2/signup', data = json.dumps(
+			'/api/v2/auth/signup', data = json.dumps(
 				self.user_data) , content_type = 'application/json')
 		result = json.loads(response.data)
-		self.assertEqual(result["message"], "User created")
+		self.assertEqual(result["message"], "New user created!")
 		self.assertEqual(response.status_code, 201)
 
 	def test_user_creation_without_all_fields(self):
+		"""Test that a user cannot be registered without filling all fields."""
 		response = self.client().post(
-			'/api/v2/signup', data = json.dumps({
+			'/api/v2/auth/signup', data = json.dumps({
 				"f_name":"peter",
 				"l_name":"peter",
 				"u_name":"peter",
@@ -49,6 +54,62 @@ class UserTestCase(unittest.TestCase):
 		self.assertEqual(result["message"], "Missing argument")
 		self.assertEqual(response.status_code, 400)
 
+	def test_already_registered_user(self):
+		"""Test that a user cannot be registered twice."""
+		response = self.client().post(
+			'/api/v2/auth/signup', data = json.dumps(
+				self.user_data) , content_type = 'application/json')
+		self.assertEqual(response.status_code, 201)
+		response = self.client().post(
+			'/api/v2/auth/signup', data = json.dumps(
+				self.user_data) , content_type = 'application/json')
+		result = json.loads(response.data)
+		self.assertEqual(result["message"], "User already exists!")
+		self.assertEqual(response.status_code, 202)
+
+	def test_user_login(self):
+		"""Test that registered user can sign in."""
+		response = self.client().post(
+			'/api/v2/auth/signup', data = json.dumps(
+				self.user_data) , content_type = 'application/json')
+		self.assertEqual(response.status_code, 201)
+		signin = self.client().post(
+			'/api/v2/auth/login', data = json.dumps({
+				'u_name': 'Pemwa',
+				'password': '12345'
+				}) , content_type = 'application/json')
+		result = json.loads(signin.data)
+		self.assertEqual(signin.status_code, 200)
+
+	def test_non_registered_user_login(self):
+		"""Test that non registered users cannot sign in"""
+		wrong_data = {
+				'u_name': 'Mwash',
+				'password': '12345'
+		}
+		response = self.client().post(
+			'/api/v2/auth/login', data = json.dumps(
+			 wrong_data), content_type = 'application/json')
+		result = json.loads(response.data)
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(result["message"], "User {} doesn\'t exist".format(
+			wrong_data['u_name']))
+
+	def test_user_login_with_wrong_password(self):
+		"""Test that users cannot sign in with wrong password"""
+		response = self.client().post(
+			'/api/v2/auth/signup', data = json.dumps(
+				self.user_data) , content_type = 'application/json')
+		self.assertEqual(response.status_code, 201)
+		signin = self.client().post(
+			'/api/v2/auth/login', data = json.dumps({
+				'u_name': 'Pemwa',
+				'password': 'wrong_abc'
+				}) , content_type = 'application/json')
+		result = json.loads(signin.data)
+		self.assertEqual(signin.status_code, 401)
+		self.assertEqual(result["message"], "Wrong password or username!")
+
 
 	def tearDown(self):
 		"""teardown all initialized variables."""
@@ -57,6 +118,7 @@ class UserTestCase(unittest.TestCase):
 			db.drop_all()
 
 class MealTestCase(unittest.TestCase):
+	"""This class represents the meals test cases"""
 
 	def setUp(self):
 		self.app = create_app("testing")
@@ -67,33 +129,83 @@ class MealTestCase(unittest.TestCase):
 					"price":400.00
 					 }
 
+
 		"""
 		Binding the app with the current context
 		and creating all tables
 		"""
 		with self.app.app_context():
+			db.session.close()
+			db.drop_all()
 			db.create_all()
 
-	def test_meal_addition(self):
+	def register_a_user(self,public_id=str(uuid.uuid4()),f_name='Peter',
+			l_name='Mwaura',u_name='Pemwa',email='pemwa@gm.com',password='12345'):
+		user_data = {
+			'public_id': str(uuid.uuid4()),
+			'f_name': 'Peter',
+			'l_name': 'Mwaura',
+			'u_name': 'Pemwa',
+			'email': 'pemwa@gm.com',
+			'password': '12345'
+		}
+		return self.client().post(
+			'/api/v2/auth/signup', data = json.dumps(
+				user_data) , content_type = 'application/json')
+
+	def login_user(self, u_name="Pemwa", password="12345"):
+		user_data = {
+			'u_name': 'Pemwa',
+			'password': '12345'
+		}
+		return self.client().post(
+			'/api/v2/auth/login', data = json.dumps(
+				user_data) , content_type = 'application/json')
+	
+	def upgrade_user(self):
+		return self.client().put('/api/v2/upgrade/1')
+
+	def test_meal_addition_to_meal_options(self):
+		"""Test API can add a meal to meal options (POST request)"""
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode('UTF-8'))['access_token']
+
 		response = self.client().post(
-			'/api/v2/meals', data = json.dumps(
+			'/api/v2/meals', 
+				headers=dict(Authorization="Bearer " + access_token),
+				data = json.dumps(
 				self.meal_data) , content_type = 'application/json')
-		self.assertEqual(response.status_code, 201)
+		
 		result = json.loads(response.data)
 		self.assertEqual(result["message"], "Meal added")
+		self.assertEqual(response.status_code, 201)
 
 	def test_meal_addition_without_one_field(self):
+
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
 		response = self.client().post(
-			'/api/v2/meals', data = json.dumps({
+			'/api/v2/meals', 
+			headers=dict(Authorization="Bearer " + access_token),
+			data = json.dumps({
 				"meal_category":"Snacks",
 				"meal_price":400.00
 				}), content_type = 'application/json')
 		result = json.loads(response.data)
-		self.assertEqual(result["message"], "Missing argument")
+		self.assertEqual(result["message"], "Missing argument!")
 		self.assertEqual(response.status_code, 400)	
 
 	def test_get_all_meals(self):
 		"""Test API can get all meals (GET request)."""
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
 		
 		response = self.client().post(
 			'/api/v2/meals', data = json.dumps(
@@ -103,6 +215,11 @@ class MealTestCase(unittest.TestCase):
 
 	def test_a_meal_can_be_edited(self):
 		"""Test API can update an existing meal option. (PUT request)"""
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
 		new_data = {
 					"m_name":"Burger",
 					"category":"Snacks",
@@ -121,6 +238,11 @@ class MealTestCase(unittest.TestCase):
 
 	def test_api_can_get_a_meal_by_id(self):
 		"""Test API can get a single meal by using it's id."""
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
 		res = self.client().post(
 			'/api/v2/meals', data= json.dumps(
 				self.meal_data), content_type = 'application/json')
@@ -130,6 +252,91 @@ class MealTestCase(unittest.TestCase):
 			'/api/v2/meals/{}'.format(result_in_json['id']))
 		self.assertEqual(result.status_code, 200)
 		self.assertIn('Burger', str(result.data))
+
+	def test_update_a_meal(self):
+		"""Test API can edit an existing meal. (PUT request)"""
+
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
+		new_data = {
+					"meal_name":"Burger",
+					"meal_category":"Snacks",
+					"meal_price":550.00
+					}
+		response = self.client().put(
+			'/api/v2/meals/1',
+			data = json.dumps(
+				new_data), content_type = 'application/json')
+		res = json.loads(response.data)
+		self.assertEqual(res["message"], "Meal updated")
+		self.assertEqual(response.status_code, 200)
+
+	def test_delete_a_meal(self):
+		"""Test API can delete an existing meal. (DELETE request)"""
+
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
+		response = self.client().delete('/api/v2/meals/1')
+		result = json.loads(response.data)
+		self.assertEqual(result["message"], "Meal deleted!")
+		self.assertEqual(response.status_code, 200)
+
+	def tearDown(self):
+		"""teardown all initialized variables."""
+		with self.app.app_context():
+			db.session.remove()
+			db.drop_all()
+
+
+	def test_make_order(self):
+		"""Test API can make an order (POST request)."""
+
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
+		response = self.client().post(
+			'/api/v2/orders', data = json.dumps(
+				self.meal_data) , content_type = 'application/json')
+		result = json.loads(response.data)
+		self.assertEqual(result["message"], "Order created")
+		self.assertEqual(response.status_code, 201)
+
+	def test_post_a_menu_for_the_day(self):
+		"""Test API can post the menu (POST request)."""
+
+		self.register_a_user()
+		self.upgrade_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
+		response = self.client().post('/api/v2/menu',
+			data = json.dumps(
+				self.meal_data), content_type = 'application/json')
+		self.assertEqual(response.status_code, 201)
+		response = self.app.get('/api/v2/orders')
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_menu_of_the_day(self):
+		"""Test API can get the menu (GET request)."""
+
+		self.register_a_user()
+		res = self.login_user()
+		access_token = json.loads(res.data.decode())['access_token']
+
+		response = self.client().post('/api/v2/menu',
+			data = json.dumps(
+				self.meal_data), content_type = 'application/json')
+		self.assertEqual(response.status_code, 201)
+		response = self.app.get('/api/v2/orders')
+		self.assertEqual(response.status_code, 200)
 
 
 	def tearDown(self):
