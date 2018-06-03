8eadadf21670041134d057cabfe5f6ceea6fe9fc
petermwash <petermwash48@gmail.com>
2018-06-03 13:06:40 +0300

    FIX: Modify table models

--- app/models.py
+++ app/models.py
@@ -18,18 +18,18 @@ class User(db.Model):
 	u_name = db.Column(db.String(75), unique=True, nullable=False)
 	email = db.Column(db.String(75), unique=True, nullable=False)
 	password = db.Column(db.String(225), nullable=False)
-	type_admin = db.Column(db.Boolean)
+	type_admin = db.Column(db.Boolean, nullable=False, default=False)
 	"""ordercart = db.relationship(
 		'Order', order_by='Order.cart_id', cascade="all, delete-orphan")"""
 
-	def __init__(self, public_id, f_name, l_name, email, u_name, password):
+	def __init__(self, public_id, f_name, l_name, email, u_name, password, type_admin=False):
 		self.public_id = public_id
 		self.f_name = f_name
 		self.l_name = l_name
 		self.email = email
 		self.u_name = u_name
 		self.password = password 
-		self.type_admin = False
+		self.type_admin = type_admin
 
 	def save(self):
 		"""Persist the user in the database"""
@@ -40,6 +40,36 @@ class User(db.Model):
 	def __repr__(self):
 		"""Return a representation of a user instance."""
 		return "<User: {}>".format(self.u_name)
+
+class BlacklistToken(db.Model):
+	"""Token Model for storing blacklisted JWT tokens"""
+
+	__tablename__ = 'blacklist_tokens'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	token = db.Column(db.String(500), unique=True, nullable=False)
+	blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+	def __init__(self, token):
+		self.token = token
+		self.blacklisted_on = datetime.now()
+
+	def add(self):
+		"""Persist the blacklisted token in the database"""
+		db.session.add(self)
+		db.session.commit()
+
+	@staticmethod
+	def check_token_blacklist(auth_token):
+		"""The method checks whwther token has been blacklisted"""
+		result = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+		if result:
+			return True
+		else:
+			return False
+
+	def __repr__(self):
+		return '<id: token: {}>'.format(self.token)
 
 
 class Meal(db.Model):
@@ -84,6 +114,7 @@ class Order(db.Model):
 	meal_name = db.Column(db.String(75), nullable=False)
 	quantity = db.Column(db.Integer)
 	owner = db.Column(db.String(75), nullable=False)
+	ordered_on = db.Column(db.DateTime, nullable=False)
 	
 	def __init__(self, meal_name, quantity, owner):
 		
@@ -92,6 +123,7 @@ class Order(db.Model):
 		self.meal_name = meal_name
 		self.quantity = quantity
 		self.owner = owner
+		self.ordered_on = datetime.now()
 
 	def save(self):
 		db.session.add(self)
@@ -117,6 +149,7 @@ class Menu(db.Model):
 	m_name = db.Column(db.String(200), nullable=False)
 	category = db.Column(db.String(200), nullable=False)
 	price = db.Column(db.Float, nullable = False)
+	day = db.Column(db.Date, default=db.func.current_timestamp())
 
 	def __init__(self, m_name, category, price):
 		self.m_name = m_name
